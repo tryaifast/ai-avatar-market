c0897253a8afd8888678823c7760cfa1eeba4571
Daniel <daniel@example.com>
2026-04-08 18:23:21 +0800

    chore: add jest configuration

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+const nextJest = require('@next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapping: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: [
+    '**/*.test.ts',
+    '**/*.test.tsx',
+  ],
+  collectCoverageFrom: [
+    'app/**/*.{ts,tsx}',
+    '!app/**/*.d.ts',
+    '!app/**/layout.tsx',
+    '!app/**/page.tsx',
+    '!app/**/loading.tsx',
+    '!app/**/error.tsx',
+    '!app/**/route.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 60,
+      lines: 60,
+      statements: 60,
+    },
+  },
+  modulePathIgnorePatterns: [
+    '<rootDir>/.next/',
+    '<rootDir>/node_modules/',
+  ],
+}
+
+module.exports = createJestConfig(customJestConfig)
